e2088ea95bb47195476e0046458a14b8800a6673
willian-mayer <willian.mayer.br@gmail.com>
2025-08-20 11:48:51 -0300

    adicionando CORS ao backend

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+    app.enableCors({
+    origin: 'http://localhost:3000', // permite solo al frontend
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
