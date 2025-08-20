44093ae7dd512709ea13ac1bbdf041f3bfc51fe1
willian-mayer <willian.mayer.br@gmail.com>
2025-08-20 12:05:31 -0300

    adicionando tailwindcss ao frontend

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-    app.enableCors({
+  app.enableCors({
     origin: 'http://localhost:3000', // permite solo al frontend
     credentials: true,
   });
